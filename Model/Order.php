77382cf77ed60c096991cdb6ad61f8c8a9729f36
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-03-23 17:53:54 +0100

    order sync command p15

--- Model/Order.php
+++ Model/Order.php
@@ -236,4 +236,632 @@ abstract class Order implements OrderInterface
      * @ORM\Column(type="text")
      */
     protected $xmlParams;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCurrencyCode()
+    {
+        return $this->currencyCode;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCurrencyCode($currencyCode)
+    {
+        $this->currencyCode = $currencyCode;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCustomer(CustomerInterface $customer = null)
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomerComment()
+    {
+        return $this->customerComment;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCustomerComment($customerComment)
+    {
+        $this->customerComment = $customerComment;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDelivery()
+    {
+        return $this->delivery;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDelivery(DeliveryInterface $delivery = null)
+    {
+        $this->delivery = $delivery;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExternalId()
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExternalId($externalId)
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGiftCertificateAmount()
+    {
+        return $this->giftCertificateAmount;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setGiftCertificateAmount($giftCertificateAmount)
+    {
+        $this->giftCertificateAmount = $giftCertificateAmount;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGiftCertificateNumber()
+    {
+        return $this->giftCertificateNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setGiftCertificateNumber($giftCertificateNumber)
+    {
+        $this->giftCertificateNumber = $giftCertificateNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIncomplete()
+    {
+        return $this->incomplete;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setIncomplete($incomplete)
+    {
+        $this->incomplete = $incomplete;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getInvoice()
+    {
+        return $this->invoice;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setInvoice(InvoiceInterface $invoice = null)
+    {
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModifiedDate()
+    {
+        return $this->modifiedDate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setModifiedDate($modifiedDate)
+    {
+        $this->modifiedDate = $modifiedDate;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addOrderLine(OrderLineInterface $orderLine)
+    {
+        $this->orderLines[] = $orderLine;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrderLines()
+    {
+        return $this->orderLines;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeOrderLine(OrderLineInterface $orderLine)
+    {
+        $this->orderLines->removeElement($orderLine);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPaymentMethod(PaymentMethodInterface $paymentMethod = null)
+    {
+        $this->paymentMethod = $paymentMethod;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReferenceNumber()
+    {
+        return $this->referenceNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReferenceNumber($referenceNumber)
+    {
+        $this->referenceNumber = $referenceNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReferrer()
+    {
+        return $this->referrer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReferrer($referrer)
+    {
+        $this->referrer = $referrer;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReservedField1()
+    {
+        return $this->reservedField1;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReservedField1($reservedField1)
+    {
+        $this->reservedField1 = $reservedField1;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReservedField2()
+    {
+        return $this->reservedField2;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReservedField2($reservedField2)
+    {
+        $this->reservedField2 = $reservedField2;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReservedField3()
+    {
+        return $this->reservedField3;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReservedField3($reservedField3)
+    {
+        $this->reservedField3 = $reservedField3;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReservedField4()
+    {
+        return $this->reservedField4;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReservedField4($reservedField4)
+    {
+        $this->reservedField4 = $reservedField4;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReservedField5()
+    {
+        return $this->reservedField5;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setReservedField5($reservedField5)
+    {
+        $this->reservedField5 = $reservedField5;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalesDiscount()
+    {
+        return $this->salesDiscount;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSalesDiscount($salesDiscount)
+    {
+        $this->salesDiscount = $salesDiscount;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getShippingMethod()
+    {
+        return $this->shippingMethod;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setShippingMethod(ShippingMethodInterface $shippingMethod = null)
+    {
+        $this->shippingMethod = $shippingMethod;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSite(SiteInterface $site = null)
+    {
+        $this->site = $site;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setState(StateInterface $state = null)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTotalPrice()
+    {
+        return $this->totalPrice;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTotalPrice($totalPrice)
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTotalWeight()
+    {
+        return $this->totalWeight;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTotalWeight($totalWeight)
+    {
+        $this->totalWeight = $totalWeight;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTrackingNumber()
+    {
+        return $this->trackingNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTrackingNumber($trackingNumber)
+    {
+        $this->trackingNumber = $trackingNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTransactionNumber()
+    {
+        return $this->transactionNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTransactionNumber($transactionNumber)
+    {
+        $this->transactionNumber = $transactionNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVatPct()
+    {
+        return $this->vatPct;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setVatPct($vatPct)
+    {
+        $this->vatPct = $vatPct;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVatRegNumber()
+    {
+        return $this->vatRegNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setVatRegNumber($vatRegNumber)
+    {
+        $this->vatRegNumber = $vatRegNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getXmlParams()
+    {
+        return $this->xmlParams;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setXmlParams($xmlParams)
+    {
+        $this->xmlParams = $xmlParams;
+
+        return $this;
+    }
 }
--- Model/OrderInterface.php
+++ Model/OrderInterface.php
@@ -4,4 +4,561 @@ namespace Loevgaard\DandomainFoundationBundle\Model;
 
 interface OrderInterface
 {
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment();
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return OrderInterface
+     */
+    public function setComment($comment);
+
+    /**
+     * Get createdDate
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate();
+
+    /**
+     * Set createdDate
+     *
+     * @param \DateTime $createdDate
+     *
+     * @return OrderInterface
+     */
+    public function setCreatedDate($createdDate);
+
+    /**
+     * Get currencyCode
+     *
+     * @return string
+     */
+    public function getCurrencyCode();
+
+    /**
+     * Set currencyCode
+     *
+     * @param string $currencyCode
+     *
+     * @return OrderInterface
+     */
+    public function setCurrencyCode($currencyCode);
+
+    /**
+     * Get customer
+     *
+     * @return CustomerInterface
+     */
+    public function getCustomer();
+
+    /**
+     * Set customer
+     *
+     * @param CustomerInterface $customer
+     *
+     * @return OrderInterface
+     */
+    public function setCustomer(CustomerInterface $customer = null);
+
+    /**
+     * Get customerComment
+     *
+     * @return string
+     */
+    public function getCustomerComment();
+
+    /**
+     * Set customerComment
+     *
+     * @param string $customerComment
+     *
+     * @return OrderInterface
+     */
+    public function setCustomerComment($customerComment);
+
+    /**
+     * Get delivery
+     *
+     * @return DeliveryInterface
+     */
+    public function getDelivery();
+
+    /**
+     * Set delivery
+     *
+     * @param DeliveryInterface $delivery
+     *
+     * @return OrderInterface
+     */
+    public function setDelivery(DeliveryInterface $delivery = null);
+
+    /**
+     * Get externalId
+     *
+     * @return integer
+     */
+    public function getExternalId();
+
+    /**
+     * Set externalId
+     *
+     * @param integer $externalId
+     *
+     * @return OrderInterface
+     */
+    public function setExternalId($externalId);
+
+    /**
+     * Get giftCertificateAmount
+     *
+     * @return string
+     */
+    public function getGiftCertificateAmount();
+
+    /**
+     * Set giftCertificateAmount
+     *
+     * @param string $giftCertificateAmount
+     *
+     * @return OrderInterface
+     */
+    public function setGiftCertificateAmount($giftCertificateAmount);
+
+    /**
+     * Get giftCertificateNumber
+     *
+     * @return integer
+     */
+    public function getGiftCertificateNumber();
+
+    /**
+     * Set giftCertificateNumber
+     *
+     * @param integer $giftCertificateNumber
+     *
+     * @return OrderInterface
+     */
+    public function setGiftCertificateNumber($giftCertificateNumber);
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId();
+
+    /**
+     * Get incomplete
+     *
+     * @return boolean
+     */
+    public function getIncomplete();
+
+    /**
+     * Set incomplete
+     *
+     * @param boolean $incomplete
+     *
+     * @return OrderInterface
+     */
+    public function setIncomplete($incomplete);
+
+    /**
+     * Get invoice
+     *
+     * @return InvoiceInterface
+     */
+    public function getInvoice();
+
+    /**
+     * Set invoice
+     *
+     * @param InvoiceInterface $invoice
+     *
+     * @return OrderInterface
+     */
+    public function setInvoice(InvoiceInterface $invoice = null);
+
+    /**
+     * Get ip
+     *
+     * @return string
+     */
+    public function getIp();
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     *
+     * @return OrderInterface
+     */
+    public function setIp($ip);
+
+    /**
+     * Get modified
+     *
+     * @return boolean
+     */
+    public function getModified();
+
+    /**
+     * Set modified
+     *
+     * @param boolean $modified
+     *
+     * @return OrderInterface
+     */
+    public function setModified($modified);
+
+    /**
+     * Get modifiedDate
+     *
+     * @return \DateTime
+     */
+    public function getModifiedDate();
+
+    /**
+     * Set modifiedDate
+     *
+     * @param \DateTime $modifiedDate
+     *
+     * @return OrderInterface
+     */
+    public function setModifiedDate($modifiedDate);
+
+    /**
+     * Add orderLine
+     *
+     * @param OrderLineInterface $orderLine
+     *
+     * @return OrderInterface
+     */
+    public function addOrderLine(OrderLineInterface $orderLine);
+
+    /**
+     * Get orderLines
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getOrderLines();
+
+    /**
+     * Remove orderLine
+     *
+     * @param OrderLineInterface $orderLine
+     */
+    public function removeOrderLine(OrderLineInterface $orderLine);
+
+    /**
+     * Get paymentMethod
+     *
+     * @return PaymentMethodInterface
+     */
+    public function getPaymentMethod();
+
+    /**
+     * Set paymentMethod
+     *
+     * @param PaymentMethodInterface $paymentMethod
+     *
+     * @return OrderInterface
+     */
+    public function setPaymentMethod(PaymentMethodInterface $paymentMethod = null);
+
+    /**
+     * Get referenceNumber
+     *
+     * @return string
+     */
+    public function getReferenceNumber();
+
+    /**
+     * Set referenceNumber
+     *
+     * @param string $referenceNumber
+     *
+     * @return OrderInterface
+     */
+    public function setReferenceNumber($referenceNumber);
+
+    /**
+     * Get referrer
+     *
+     * @return string
+     */
+    public function getReferrer();
+
+    /**
+     * Set referrer
+     *
+     * @param string $referrer
+     *
+     * @return OrderInterface
+     */
+    public function setReferrer($referrer);
+
+    /**
+     * Get reservedField1
+     *
+     * @return string
+     */
+    public function getReservedField1();
+
+    /**
+     * Set reservedField1
+     *
+     * @param string $reservedField1
+     *
+     * @return OrderInterface
+     */
+    public function setReservedField1($reservedField1);
+
+    /**
+     * Get reservedField2
+     *
+     * @return string
+     */
+    public function getReservedField2();
+
+    /**
+     * Set reservedField2
+     *
+     * @param string $reservedField2
+     *
+     * @return OrderInterface
+     */
+    public function setReservedField2($reservedField2);
+
+    /**
+     * Get reservedField3
+     *
+     * @return string
+     */
+    public function getReservedField3();
+
+    /**
+     * Set reservedField3
+     *
+     * @param string $reservedField3
+     *
+     * @return OrderInterface
+     */
+    public function setReservedField3($reservedField3);
+
+    /**
+     * Get reservedField4
+     *
+     * @return string
+     */
+    public function getReservedField4();
+
+    /**
+     * Set reservedField4
+     *
+     * @param string $reservedField4
+     *
+     * @return OrderInterface
+     */
+    public function setReservedField4($reservedField4);
+
+    /**
+     * Get reservedField5
+     *
+     * @return string
+     */
+    public function getReservedField5();
+
+    /**
+     * Set reservedField5
+     *
+     * @param string $reservedField5
+     *
+     * @return OrderInterface
+     */
+    public function setReservedField5($reservedField5);
+
+    /**
+     * Get salesDiscount
+     *
+     * @return string
+     */
+    public function getSalesDiscount();
+
+    /**
+     * Set salesDiscount
+     *
+     * @param string $salesDiscount
+     *
+     * @return OrderInterface
+     */
+    public function setSalesDiscount($salesDiscount);
+
+    /**
+     * Get shippingMethod
+     *
+     * @return ShippingMethodInterface
+     */
+    public function getShippingMethod();
+
+    /**
+     * Set shippingMethod
+     *
+     * @param ShippingMethodInterface $shippingMethod
+     *
+     * @return OrderInterface
+     */
+    public function setShippingMethod(ShippingMethodInterface $shippingMethod = null);
+
+    /**
+     * Get site
+     *
+     * @return SiteInterface
+     */
+    public function getSite();
+
+    /**
+     * Set site
+     *
+     * @param SiteInterface $site
+     *
+     * @return OrderInterface
+     */
+    public function setSite(SiteInterface $site = null);
+
+    /**
+     * Get state
+     *
+     * @return StateInterface
+     */
+    public function getState();
+
+    /**
+     * Set state
+     *
+     * @param StateInterface $state
+     *
+     * @return OrderInterface
+     */
+    public function setState(StateInterface $state = null);
+
+    /**
+     * Get totalPrice
+     *
+     * @return string
+     */
+    public function getTotalPrice();
+
+    /**
+     * Set totalPrice
+     *
+     * @param string $totalPrice
+     *
+     * @return OrderInterface
+     */
+    public function setTotalPrice($totalPrice);
+
+    /**
+     * Get totalWeight
+     *
+     * @return string
+     */
+    public function getTotalWeight();
+
+    /**
+     * Set totalWeight
+     *
+     * @param string $totalWeight
+     *
+     * @return OrderInterface
+     */
+    public function setTotalWeight($totalWeight);
+
+    /**
+     * Get trackingNumber
+     *
+     * @return string
+     */
+    public function getTrackingNumber();
+
+    /**
+     * Set trackingNumber
+     *
+     * @param string $trackingNumber
+     *
+     * @return OrderInterface
+     */
+    public function setTrackingNumber($trackingNumber);
+
+    /**
+     * Get transactionNumber
+     *
+     * @return string
+     */
+    public function getTransactionNumber();
+
+    /**
+     * Set transactionNumber
+     *
+     * @param string $transactionNumber
+     *
+     * @return OrderInterface
+     */
+    public function setTransactionNumber($transactionNumber);
+
+    /**
+     * Get vatPct
+     *
+     * @return string
+     */
+    public function getVatPct();
+
+    /**
+     * Set vatPct
+     *
+     * @param string $vatPct
+     *
+     * @return OrderInterface
+     */
+    public function setVatPct($vatPct);
+
+    /**
+     * Get vatRegNumber
+     *
+     * @return string
+     */
+    public function getVatRegNumber();
+
+    /**
+     * Set vatRegNumber
+     *
+     * @param string $vatRegNumber
+     *
+     * @return OrderInterface
+     */
+    public function setVatRegNumber($vatRegNumber);
+
+    /**
+     * Get xmlParams
+     *
+     * @return string
+     */
+    public function getXmlParams();
+
+    /**
+     * Set xmlParams
+     *
+     * @param string $xmlParams
+     *
+     * @return OrderInterface
+     */
+    public function setXmlParams($xmlParams);
 }
