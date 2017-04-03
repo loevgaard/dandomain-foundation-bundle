d1fbf52343dbc4b928a1cad176c0a8b06441d0fa
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-03 17:36:08 +0200

    order sync command p38

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -41,8 +41,8 @@ class OrderService
     /**
      * Synchronizates orders.
      *
-     * @param string               $end
-     * @param string     $start
+     * @param string $end
+     * @param string $start
      */
     public function orderSync($end = null, $start = null)
     {
@@ -52,13 +52,21 @@ class OrderService
         if (null !== $start) {
             $start = new \DateTime($start);
         }
+
+        if (null === $end) {
+        
+        }
+        //$start->setTime(0, 0, 0);
+        //$end->setTime(23, 59, 59);
 var_dump($start);
 var_dump($end);
+/*
         $orders = GuzzleHttp\json_decode($this->api->order->getOrdersInModifiedInterval($start, $end)->getBody()->getContents());
 var_dump(count($orders));
         foreach ($orders as $order) {
             $this->orderSynchronizer->syncOrder($order, true);
             throw new \Exception('aaa');
         }
+*/
     }
 }
