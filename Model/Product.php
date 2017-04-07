0cf9ae0d863a17b3a5a893e7420eeca0228b3c26
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-07 15:23:35 +0200

    product sync command p3

--- Model/Product.php
+++ Model/Product.php
@@ -54,6 +54,54 @@ abstract class Product implements ProductInterface
      */
     protected $createdBy;
 
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $defaultCategoryId;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(nullable=true, type="array")
+     */
+    protected $disabledVariantIdList;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(nullable=true, type="array")
+     */
+    protected $disabledVariants;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(nullable=true, type="string")
+     */
+    protected $edbPriserProductNumber;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $externalId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(nullable=true, type="string")
+     */
+    protected $fileSaleLink;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(nullable=true, type="string")
+     */
+    protected $googleFeedCategory;
 
 
 }
