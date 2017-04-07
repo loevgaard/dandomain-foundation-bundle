db52911c09c3b34e2a7ccda10c42cfc5a1d3c483
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-07 11:32:29 +0200

    category sync command p4

--- Model/Category.php
+++ Model/Category.php
@@ -94,13 +94,6 @@ abstract class Category implements CategoryInterface
     protected $modified;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(nullable=true, type="string")
-     */
-    protected $number;
-
-    /**
      * @var array
      *
      * @ORM\Column(nullable=true, type="array")
@@ -224,5 +217,535 @@ abstract class Category implements CategoryInterface
      *
      * @ORM\Column(nullable=true, type="string")
      */
-    protected $urlname;
+    protected $textUrlname;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getB2bGroupId()
+    {
+        return $this->b2bGroupId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setB2bGroupId($b2bGroupId)
+    {
+        $this->b2bGroupId = $b2bGroupId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedDate()
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatedDate($createdDate)
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomInfoLayout()
+    {
+        return $this->customInfoLayout;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCustomInfoLayout($customInfoLayout)
+    {
+        $this->customInfoLayout = $customInfoLayout;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomListLayout()
+    {
+        return $this->customListLayout;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCustomListLayout($customListLayout)
+    {
+        $this->customListLayout = $customListLayout;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultParentId()
+    {
+        return $this->defaultParentId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultParentId($defaultParentId)
+    {
+        $this->defaultParentId = $defaultParentId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEditedDate()
+    {
+        return $this->editedDate;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEditedDate($editedDate)
+    {
+        $this->editedDate = $editedDate;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExternalId()
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExternalId($externalId)
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getInfoLayout()
+    {
+        return $this->infoLayout;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setInfoLayout($infoLayout)
+    {
+        $this->infoLayout = $infoLayout;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getInternalId()
+    {
+        return $this->internalId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setInternalId($internalId)
+    {
+        $this->internalId = $internalId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getListLayout()
+    {
+        return $this->listLayout;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setListLayout($listLayout)
+    {
+        $this->listLayout = $listLayout;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParentIdList()
+    {
+        return $this->parentIdList;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setParentIdList($parentIdList)
+    {
+        $this->parentIdList = $parentIdList;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSegmentIdList()
+    {
+        return $this->segmentIdList;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSegmentIdList($segmentIdList)
+    {
+        $this->segmentIdList = $segmentIdList;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextKeywords()
+    {
+        return $this->textKeywords;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextKeywords($textKeywords)
+    {
+        $this->textKeywords = $textKeywords;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextCategoryNumber()
+    {
+        return $this->textCategoryNumber;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextCategoryNumber($textCategoryNumber)
+    {
+        $this->textCategoryNumber = $textCategoryNumber;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextDescription()
+    {
+        return $this->textDescription;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextDescription($textDescription)
+    {
+        $this->textDescription = $textDescription;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextExternalId()
+    {
+        return $this->textExternalId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextExternalId($textExternalId)
+    {
+        $this->textExternalId = $textExternalId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextHidden()
+    {
+        return $this->textHidden;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextHidden($textHidden)
+    {
+        $this->textHidden = $textHidden;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextHiddenMobile()
+    {
+        return $this->textHiddenMobile;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextHiddenMobile($textHiddenMobile)
+    {
+        $this->textHiddenMobile = $textHiddenMobile;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextIcon()
+    {
+        return $this->textIcon;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextIcon($textIcon)
+    {
+        $this->textIcon = $textIcon;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextImage()
+    {
+        return $this->textImage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextImage($textImage)
+    {
+        $this->textImage = $textImage;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextLink()
+    {
+        return $this->textLink;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextLink($textLink)
+    {
+        $this->textLink = $textLink;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextMetaDescription()
+    {
+        return $this->textMetaDescription;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextMetaDescription($textMetaDescription)
+    {
+        $this->textMetaDescription = $textMetaDescription;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextName()
+    {
+        return $this->textName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextName($textName)
+    {
+        $this->textName = $textName;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextSiteId()
+    {
+        return $this->textSiteId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextSiteId($textSiteId)
+    {
+        $this->textSiteId = $textSiteId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextSortOrder()
+    {
+        return $this->textSortOrder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextSortOrder($textSortOrder)
+    {
+        $this->textSortOrder = $textSortOrder;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextString()
+    {
+        return $this->textString;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextString($textString)
+    {
+        $this->textString = $textString;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextTitle()
+    {
+        return $this->textTitle;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextTitle($textTitle)
+    {
+        $this->textTitle = $textTitle;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTextUrlname()
+    {
+        return $this->textUrlname;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTextUrlname($textUrlname)
+    {
+        $this->textUrlname = $textUrlname;
+
+        return $this;
+    }
 }
--- Model/CategoryInterface.php
+++ Model/CategoryInterface.php
@@ -4,5 +4,474 @@ namespace Loevgaard\DandomainFoundationBundle\Model;
 
 interface CategoryInterface
 {
+    /**
+     * Get b2bGroupId
+     *
+     * @return string
+     */
+    public function getB2bGroupId();
 
+    /**
+     * Set b2bGroupId
+     *
+     * @param string $b2bGroupId
+     *
+     * @return CategoryInterface
+     */
+    public function setB2bGroupId($b2bGroupId);
+
+    /**
+     * Get createdDate
+     *
+     * @return \DateTime
+     */
+    public function getCreatedDate();
+
+    /**
+     * Set createdDate
+     *
+     * @param \DateTime $createdDate
+     *
+     * @return CategoryInterface
+     */
+    public function setCreatedDate($createdDate);
+
+    /**
+     * Get customInfoLayout
+     *
+     * @return integer
+     */
+    public function getCustomInfoLayout();
+
+    /**
+     * Set customInfoLayout
+     *
+     * @param integer $customInfoLayout
+     *
+     * @return CategoryInterface
+     */
+    public function setCustomInfoLayout($customInfoLayout);
+
+    /**
+     * Get customListLayout
+     *
+     * @return integer
+     */
+    public function getCustomListLayout();
+
+    /**
+     * Set customListLayout
+     *
+     * @param integer $customListLayout
+     *
+     * @return CategoryInterface
+     */
+    public function setCustomListLayout($customListLayout);
+
+    /**
+     * Get defaultParentId
+     *
+     * @return integer
+     */
+    public function getDefaultParentId();
+
+    /**
+     * Set defaultParentId
+     *
+     * @param integer $defaultParentId
+     *
+     * @return CategoryInterface
+     */
+    public function setDefaultParentId($defaultParentId);
+
+    /**
+     * Get editedDate
+     *
+     * @return \DateTime
+     */
+    public function getEditedDate();
+
+    /**
+     * Set editedDate
+     *
+     * @param \DateTime $editedDate
+     *
+     * @return CategoryInterface
+     */
+    public function setEditedDate($editedDate);
+
+    /**
+     * Get externalId
+     *
+     * @return integer
+     */
+    public function getExternalId();
+
+    /**
+     * Set externalId
+     *
+     * @param integer $externalId
+     *
+     * @return CategoryInterface
+     */
+    public function setExternalId($externalId);
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId();
+
+    /**
+     * Get infoLayout
+     *
+     * @return integer
+     */
+    public function getInfoLayout();
+
+    /**
+     * Set infoLayout
+     *
+     * @param integer $infoLayout
+     *
+     * @return CategoryInterface
+     */
+    public function setInfoLayout($infoLayout);
+
+    /**
+     * Get internalId
+     *
+     * @return integer
+     */
+    public function getInternalId();
+
+    /**
+     * Set internalId
+     *
+     * @param integer $internalId
+     *
+     * @return CategoryInterface
+     */
+    public function setInternalId($internalId);
+
+    /**
+     * Get listLayout
+     *
+     * @return integer
+     */
+    public function getListLayout();
+
+    /**
+     * Set listLayout
+     *
+     * @param integer $listLayout
+     *
+     * @return CategoryInterface
+     */
+    public function setListLayout($listLayout);
+
+    /**
+     * Get modified
+     *
+     * @return boolean
+     */
+    public function getModified();
+
+    /**
+     * Set modified
+     *
+     * @param boolean $modified
+     *
+     * @return CategoryInterface
+     */
+    public function setModified($modified);
+
+    /**
+     * Get parentIdList
+     *
+     * @return array
+     */
+    public function getParentIdList();
+
+    /**
+     * Set parentIdList
+     *
+     * @param array $parentIdList
+     *
+     * @return CategoryInterface
+     */
+    public function setParentIdList($parentIdList);
+
+    /**
+     * Get segmentIdList
+     *
+     * @return array
+     */
+    public function getSegmentIdList();
+
+    /**
+     * Set segmentIdList
+     *
+     * @param array $segmentIdList
+     *
+     * @return CategoryInterface
+     */
+    public function setSegmentIdList($segmentIdList);
+
+    /**
+     * Get textKeywords
+     *
+     * @return string
+     */
+    public function getTextKeywords();
+
+    /**
+     * Set textKeywords
+     *
+     * @param string $textKeywords
+     *
+     * @return CategoryInterface
+     */
+    public function setTextKeywords($textKeywords);
+
+    /**
+     * Get textCategoryInterfaceNumber
+     *
+     * @return string
+     */
+    public function getTextCategoryInterfaceNumber();
+
+    /**
+     * Set textCategoryInterfaceNumber
+     *
+     * @param string $textCategoryInterfaceNumber
+     *
+     * @return CategoryInterface
+     */
+    public function setTextCategoryInterfaceNumber($textCategoryInterfaceNumber);
+
+    /**
+     * Get textDescription
+     *
+     * @return string
+     */
+    public function getTextDescription();
+
+    /**
+     * Set textDescription
+     *
+     * @param string $textDescription
+     *
+     * @return CategoryInterface
+     */
+    public function setTextDescription($textDescription);
+
+    /**
+     * Get textExternalId
+     *
+     * @return integer
+     */
+    public function getTextExternalId();
+
+    /**
+     * Set textExternalId
+     *
+     * @param integer $textExternalId
+     *
+     * @return CategoryInterface
+     */
+    public function setTextExternalId($textExternalId);
+
+    /**
+     * Get textHidden
+     *
+     * @return boolean
+     */
+    public function getTextHidden();
+
+    /**
+     * Set textHidden
+     *
+     * @param boolean $textHidden
+     *
+     * @return CategoryInterface
+     */
+    public function setTextHidden($textHidden);
+
+    /**
+     * Get textHiddenMobile
+     *
+     * @return boolean
+     */
+    public function getTextHiddenMobile();
+
+    /**
+     * Set textHiddenMobile
+     *
+     * @param boolean $textHiddenMobile
+     *
+     * @return CategoryInterface
+     */
+    public function setTextHiddenMobile($textHiddenMobile);
+
+    /**
+     * Get textIcon
+     *
+     * @return string
+     */
+    public function getTextIcon();
+
+    /**
+     * Set textIcon
+     *
+     * @param string $textIcon
+     *
+     * @return CategoryInterface
+     */
+    public function setTextIcon($textIcon);
+
+    /**
+     * Get textImage
+     *
+     * @return string
+     */
+    public function getTextImage();
+
+    /**
+     * Set textImage
+     *
+     * @param string $textImage
+     *
+     * @return CategoryInterface
+     */
+    public function setTextImage($textImage);
+
+    /**
+     * Get textLink
+     *
+     * @return string
+     */
+    public function getTextLink();
+
+    /**
+     * Set textLink
+     *
+     * @param string $textLink
+     *
+     * @return CategoryInterface
+     */
+    public function setTextLink($textLink);
+
+    /**
+     * Get textMetaDescription
+     *
+     * @return string
+     */
+    public function getTextMetaDescription();
+
+    /**
+     * Set textMetaDescription
+     *
+     * @param string $textMetaDescription
+     *
+     * @return CategoryInterface
+     */
+    public function setTextMetaDescription($textMetaDescription);
+
+    /**
+     * Get textName
+     *
+     * @return string
+     */
+    public function getTextName();
+
+    /**
+     * Set textName
+     *
+     * @param string $textName
+     *
+     * @return CategoryInterface
+     */
+    public function setTextName($textName);
+
+    /**
+     * Get textSiteId
+     *
+     * @return integer
+     */
+    public function getTextSiteId();
+
+    /**
+     * Set textSiteId
+     *
+     * @param integer $textSiteId
+     *
+     * @return CategoryInterface
+     */
+    public function setTextSiteId($textSiteId);
+
+    /**
+     * Get textSortOrder
+     *
+     * @return integer
+     */
+    public function getTextSortOrder();
+
+    /**
+     * Set textSortOrder
+     *
+     * @param integer $textSortOrder
+     *
+     * @return CategoryInterface
+     */
+    public function setTextSortOrder($textSortOrder);
+
+    /**
+     * Get textString
+     *
+     * @return string
+     */
+    public function getTextString();
+
+    /**
+     * Set textString
+     *
+     * @param string $textString
+     *
+     * @return CategoryInterface
+     */
+    public function setTextString($textString);
+
+    /**
+     * Get textTitle
+     *
+     * @return string
+     */
+    public function getTextTitle();
+
+    /**
+     * Set textTitle
+     *
+     * @param string $textTitle
+     *
+     * @return CategoryInterface
+     */
+    public function setTextTitle($textTitle);
+
+    /**
+     * Get textUrlname
+     *
+     * @return string
+     */
+    public function getTextUrlname();
+
+    /**
+     * Set textUrlname
+     *
+     * @param string $textUrlname
+     *
+     * @return CategoryInterface
+     */
+    public function setTextUrlname($textUrlname);
 }
