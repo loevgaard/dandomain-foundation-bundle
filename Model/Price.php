51337d2a2fdd0ed102b58d490b338fd09a1d1b9f
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-07 18:14:27 +0200

    product sync command p8

--- Model/Price.php
+++ Model/Price.php
@@ -14,5 +14,64 @@ abstract class Price implements PriceInterface
      */
     protected $id;
 
+    /**
+     * @var Period
+     */
+    protected $period;
 
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $amount;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $avance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(nullable=true, type="string")
+     */
+    protected $b2bGroupId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(nullable=true, type="string")
+     */
+    protected $currencyCode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $isoCode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(nullable=true, type="integer")
+     */
+    protected $periodId;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(nullable=true, type="decimal")
+     */
+    protected $specialOfferPrice;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(nullable=true, type="decimal")
+     */
+    protected $unitPrice;
 }
