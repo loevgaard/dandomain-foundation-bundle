0b906fdc5b050ef3e150d1737f4733fe7efe4c1c
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-08 11:25:27 +0200

    product sync command p10

--- Model/Variant.php
+++ Model/Variant.php
@@ -34,4 +34,66 @@ abstract class Variant implements VariantInterface
      * @ORM\Column(nullable=true, type="string")
      */
     protected $text;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExternalId()
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setExternalId($externalId)
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSortOrder()
+    {
+        return $this->sortOrder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSortOrder($sortOrder)
+    {
+        $this->sortOrder = $sortOrder;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
 }
--- Model/VariantInterface.php
+++ Model/VariantInterface.php
@@ -4,4 +4,58 @@ namespace Loevgaard\DandomainFoundationBundle\Model;
 
 interface VariantInterface
 {
+    /**
+     * Get externalId
+     *
+     * @return integer
+     */
+    public function getExternalId();
+
+    /**
+     * Set externalId
+     *
+     * @param integer $externalId
+     *
+     * @return Variant
+     */
+    public function setExternalId($externalId);
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId();
+
+    /**
+     * Get sortOrder
+     *
+     * @return integer
+     */
+    public function getSortOrder();
+
+    /**
+     * Set sortOrder
+     *
+     * @param integer $sortOrder
+     *
+     * @return Variant
+     */
+    public function setSortOrder($sortOrder);
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText();
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Variant
+     */
+    public function setText($text);
 }
