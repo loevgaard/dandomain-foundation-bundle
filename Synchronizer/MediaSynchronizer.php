bf06a32851030e6f5829c8d5c08d509a8a7195d6
Michal Piotrowski <michal@eventhorizonlabs.eu>
2017-04-28 18:16:56 +0200

    product sync command p103

---
+++ Synchronizer/MediaSynchronizer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Loevgaard\DandomainFoundationBundle\Synchronizer;
+
+use Loevgaard\DandomainFoundationBundle\Model\MediaInterface;
+
+class MediaSynchronizer extends Synchronizer
+{
+    /**
+     * @var string
+     */
+    protected $entityClassName = 'Loevgaard\\DandomainFoundationBundle\\Model\\Media';
+
+    /**
+     * @var string
+     */
+    protected $entityInterfaceName = 'Loevgaard\\DandomainFoundationBundle\\Model\\MediaInterface';
+
+    /**
+     * Synchronizes Media.
+     *
+     * @param array $media
+     * @param bool  $flush
+     *
+     * @return MediaInterface
+     */
+    public function syncMedia($media, $flush = true)
+    {
+        $entity = $this->objectManager->getRepository($this->entityClassName)->findOneBy([
+            'externalId' => $media->id,
+        ]);
+
+        if (!($entity)) {
+            $entity = new $this->entityClassName();
+        }
+
+        $entity
+            ->setExternalId($media->id)
+            ->setHeight($media->height)
+            ->setMediaTranslations($media->mediaTranslations)
+            ->setSortorder($media->sortorder)
+            ->setThumbnail($media->thumbnail)
+            ->setThumbnailheight($media->thumbnailheight)
+            ->setThumbnailwidth($media->thumbnailwidth)
+            ->setUrl($media->url)
+            ->setWidth($media->width)
+        ;
+
+        $this->objectManager->persist($entity);
+
+        if (true === $flush) {
+            $this->objectManager->flush();
+        }
+
+        return $entity;
+    }
+}
