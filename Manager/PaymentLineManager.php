9734dfdd31c5815ba1643baf0dd59212f9e007df
Joachim Løvgaard <joachim@loevgaard.dk>
2017-08-25 14:48:25 +0200

    added payment manager added payment line manager

---
+++ Manager/PaymentLineManager.php
@@ -0,0 +1,13 @@
+<?php
+namespace Loevgaard\DandomainFoundationBundle\Manager;
+
+use Loevgaard\DandomainFoundationBundle\Model\PaymentLine;
+
+/**
+ * @method PaymentLine create()
+ * @method delete(PaymentLine $obj)
+ * @method update(PaymentLine $obj, $flush = true)
+ */
+class PaymentLineManager extends Manager
+{
+}
---
+++ Manager/PaymentManager.php
@@ -0,0 +1,13 @@
+<?php
+namespace Loevgaard\DandomainFoundationBundle\Manager;
+
+use Loevgaard\DandomainFoundationBundle\Model\Payment;
+
+/**
+ * @method Payment create()
+ * @method delete(Payment $obj)
+ * @method update(Payment $obj, $flush = true)
+ */
+class PaymentManager extends Manager
+{
+}
