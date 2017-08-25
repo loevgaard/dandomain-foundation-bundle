33f1de550f406fb0a49f8c9875fc2068e9e4e486
Joachim Løvgaard <joachim@loevgaard.dk>
2017-08-25 15:37:55 +0200

    fixed bug

--- Manager/PaymentManager.php
+++ Manager/PaymentManager.php
@@ -12,12 +12,34 @@ use Loevgaard\DandomainFoundationBundle\Model\Payment;
 class PaymentManager extends Manager
 {
     /**
+     * This will transform a PaymentRequest (parent) to a Payment (child)
+     *
      * @param PaymentRequest $paymentRequest
      * @return Payment
      */
     public function createPaymentFromDandomainPaymentRequest(PaymentRequest $paymentRequest)
     {
-        $payment = Payment::createFromPaymentRequest($paymentRequest);
+        $payment = $this->create();
+
+        $methods = get_class_methods($paymentRequest);
+
+        foreach ($methods as $method) {
+            if(substr($method, 0, 3) === 'get') {
+                $val = $paymentRequest->{$method}();
+                $property = substr($method, 3);
+            } elseif (substr($method, 0, 2) === 'is') {
+                $val = $paymentRequest->{$method}();
+                $property = substr($method, 2);
+            } else {
+                continue;
+            }
+
+            $setter = 'set'.$property;
+
+            if(method_exists($payment, $setter)) {
+                $payment->{$setter}($val);
+            }
+        }
 
         return $payment;
     }
