d01085559ac9a91681ffec83ae88f6ad43ae37cc
Joachim Løvgaard <joachim@loevgaard.dk>
2017-08-28 15:15:34 +0200

    added helper method for handling date periods

--- functions.php
+++ functions.php
@@ -21,3 +21,49 @@ function jsonDateToDate($date) {
     // timestamps will always be given in the UTC time zone
     return DateTimeImmutable::createFromTimestamp('@' . $timestamp);
 }
+
+/**
+ * @param \DateTimeImmutable $startDate
+ * @param \DateTimeImmutable $endDate
+ * @param \DateInterval $interval
+ * @return \Generator|array
+ */
+function datePeriodSteps(\DateTimeImmutable $startDate, \DateTimeImmutable $endDate, \DateInterval $interval) : \Generator
+{
+    if($startDate > $endDate) {
+        throw new \InvalidArgumentException('Start date is after end date');
+    }
+
+    $steps = new \DatePeriod($startDate, $interval, $endDate);
+    $first = true;
+
+    foreach ($steps as $startStep) {
+        /** @var \DateTimeImmutable $startStep */
+        $endStep = $startStep->add($interval);
+
+        /**
+         * If it's the second round of the loop we want to add 1 second to the start step
+         * This will have the effect that there are no overlapping periods
+         */
+        if($first) {
+            $first = false;
+        } else {
+            $startStep = $startStep->add(new \DateInterval('PT1S'));
+        }
+
+        // if the end step is higher than the end date we change the end step
+        if ($endStep > $endDate) {
+            $endStep = $endDate;
+        }
+
+        // if the start step is after the end step we just continue, which practically means that the loop ends
+        if($startStep > $endStep) {
+            continue;
+        }
+
+        yield [
+            'startStep' => $startStep,
+            'endStep' => $endStep
+        ];
+    }
+}
