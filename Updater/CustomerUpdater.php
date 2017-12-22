b7d5cd8841c961d0d17e226971d6aaa26938f46d
Joachim Løvgaard <joachim@loevgaard.dk>
2017-12-22 13:38:32 +0100

    coding

--- Updater/CustomerUpdater.php
+++ Updater/CustomerUpdater.php
@@ -27,10 +27,14 @@ class CustomerUpdater implements CustomerUpdaterInterface
      */
     public function updateFromEmbeddedApiResponse(array $data): CustomerInterface
     {
-        $customer = $this->customerRepository->findOneByExternalId((int)$data['id']);
+        // we cast to int because for some reason Dandomain can (sometimes) have customers where the id is not set
+        // and instead of setting the id to 0, Dandomain sets it to ''
+        $id = (int)$data['id'];
+
+        $customer = $this->customerRepository->findOneByExternalId($id);
         if (!$customer) {
             $customer = new Customer();
-            $customer->setExternalId($data['id']);
+            $customer->setExternalId($id);
         }
 
         $customer
