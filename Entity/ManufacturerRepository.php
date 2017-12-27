5157e93a67218d69f2fa51dbbeba7d4795d7048b
Joachim Løvgaard <joachim@loevgaard.dk>
2017-12-27 16:47:22 +0100

    coding

--- Entity/ManufacturerRepository.php
+++ Entity/ManufacturerRepository.php
@@ -4,6 +4,12 @@ namespace Loevgaard\DandomainFoundationBundle\Entity;
 
 use Loevgaard\DandomainFoundation\Entity\Generated\ManufacturerInterface;
 
+/**
+ * @method null|ManufacturerInterface find($id)
+ * @method ManufacturerInterface[] findBy(array $criteria, array $orderBy = null, int $limit = null, int $offset = null)
+ * @method null|ManufacturerInterface findOneBy(array $criteria)
+ * @method ManufacturerInterface[] findAll()
+ */
 class ManufacturerRepository extends Repository implements ManufacturerRepositoryInterface
 {
     public function findOneByExternalId(string $externalId): ?ManufacturerInterface
